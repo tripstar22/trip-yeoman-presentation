01fdc2b06ce231f4335c20f77734c7b41bf59d19
Trip Pruitt <trip@brainjocks.com>
2015-10-10 13:47:05 -0400

    changes to make fullpage layout

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,27 +1,33 @@
 'use strict';
 
-// call all custom functions
-function init() {
+$(document).ready(function() {
+    // call all custom functions
+    function init() {
 
-    sectionHeight();
+        // sectionHeight();
 
-    // fittext calls on h1 and h2
-    $("h1").fitText(1.2, { minFontSize: '24px', maxFontSize: '68px' });
-    $("h2").fitText(1.2, { minFontSize: '22px', maxFontSize: '50px' });
+        // full page scrolling sections
+        $('#fullpage').fullpage();
 
-    console.log("init works!");
-}
+        // fittext calls on h1 and h2
+        $("h1").fitText(1.2, { minFontSize: '24px', maxFontSize: '68px' });
+        $("h2").fitText(1.2, { minFontSize: '22px', maxFontSize: '50px' });
 
-init();
-
-// make sections window height if window height greater than 699px
-function sectionHeight() {
-
-    var windowHeight = $(window).height();
-
-    if ($(window.height > 699)) {
-
-      $("section").css("height", windowHeight);
-      $("main").css("top", windowHeight);
+        console.log("init works!");
     }
-}
+
+    init();
+
+    // make sections window height if window height greater than 699px
+    function sectionHeight() {
+
+        var windowHeight = $(window).height();
+
+        if ($(window.height > 699)) {
+
+          $("section").css("height", windowHeight);
+          $("main").css("top", windowHeight);
+        }
+    }
+
+});
